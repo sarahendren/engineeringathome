1fb1e1bba954fb6366bb33b0f35979cd67fcec6a
Casey Gollan <hello@caseyagollan.com>
2015-12-23 19:33:26 -0500

    update home template to match default
    for now

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,13 +3,8 @@
   <main class="main" role="main">
 
     <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
+      <?php echo $page->text()->footnotes()->kirbytext() ?>
     </div>
-
-    <hr>
-
-    <?php snippet('projects') ?>
 
   </main>
 
