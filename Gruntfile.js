f6fbaaea72255ae5f0a27f83458bfc14d925bf99
Casey Gollan <casey@bullshit.systems>
2016-01-02 21:34:54 -0500

    frontend pass

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,34 @@ module.exports = function(grunt) {
         }
       }
     },
+    sprite:{
+      all: {
+        src: 'content/home/*.png',
+        dest: 'assets/adaptations-sprite.png',
+        destCss: 'assets/scss/sprites.scss',
+        padding: 2,
+        cssTemplate: "assets/sprites.css.handlebars"
+     }
+    },
+    image: {
+      static: {
+        options: {
+          pngquant: true,
+          optipng: true,
+          zopflipng: true,
+          advpng: true,
+          jpegRecompress: false,
+          jpegoptim: false,
+          mozjpeg: false,
+          gifsicle: true,
+          svgo: false
+        },
+        files: {
+          'assets/adaptations-sprite-min.png': 'assets/adaptations-sprite.png',
+          'content/1-adaptations/*/thumbnail.gif': 'content/1-adaptations/*/thumbnail.gif'
+        }
+      }
+    },
     php: {
         dist: {
             options: {
@@ -22,7 +50,7 @@ module.exports = function(grunt) {
             }
         },
         watch: {}
-   },
+    },
     watch: {
       css: {
         files: '**/*.scss',
@@ -38,5 +66,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-php');
+  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.loadNpmTasks('grunt-image');
   grunt.registerTask('default',['php:watch', 'watch']);
 }
--- site/templates/adaptations.php
+++ site/templates/adaptations.php
@@ -4,17 +4,96 @@
   <main class="main" role="main">
 	<header>
 		<div class="row">
-		    <h1><?php echo $page->title()->html() ?></h1>
+		    <h1>to date, <strong>cindy has engineered <?php echo $page->children()->count() ?> adaptations</strong>, built of things like cable ties, cosmetic sponges, peel-and-stick hooks, and more.</h1>
 	    </div>
-	    <div class="row">
-		    <div class="lead col-md-6 col-md-offset-3"><?php echo $page->text()->kirbytext() ?></div>
-		</div>
+	    <section id="filters">
+		    <div class="row">
+			    <p class="text-center lead"><em>Filter adaptations by selecting a verb below.</em></p>
+			</div>
+			<div class="row">
+				<?php $tagcloud = tagcloud(page('adaptations'), array('field' => 'verbs', 'param' => 'verb')) ?>
+				<div class="filter-button-group">
+					<?php foreach($tagcloud as $tag): ?>
+						<button class="<?php echo $tag->name() ?>" data-filter=".<?php echo $tag->name() ?>"><?php echo $tag->name() ?></button>
+					<?php endforeach ?>
+				</div>
+			</div>
+	    </section>
 	</header>
+	<div class="row">
+		<div class="col-xs-12 grid grid js-isotope">
+		  <?php foreach(page('adaptations')->children() as $adaptation):?>
+			  <div class="col-xs-6 col-sm-4 col-md-3 element-item <?php foreach($adaptation->verbs()->split(',') as $verb): echo $verb . ' '; endforeach; ?>">
+			    <a href="<?php echo $adaptation->url() ?>">
+				  	<?php if($adaptation->images()->find('thumbnail.gif')): ?>
+						<img src="<?php echo $adaptation->images()->find('thumbnail.gif')->url() ?>" class="<?php echo $adaptation->slug() ?> gif" alt="<?php echo $adaptation->title()->html() ?>">
+				  	<?php else: ?>
+					    <div class="responsive-sprites">
+					    	<img src="/assets/adaptations-sprite-min.png" class="<?php echo $adaptation->slug() ?>" alt="<?php echo $adaptation->title()->html() ?>">
+					    </div>
+					<?php endif ?>
+			    	<h2><?php echo $adaptation->title()->widont() ?></h2>
+			    </a>
+			  </div>
+		  <?php endforeach ?>
+		</div>
+	</div>
+	<script type="text/javascript">
+	$('.grid a').hover(
+		function(myClass){ $(this).find('img').addClass('silhouette') },
+		function(myClass){ $(this).find('img').removeClass('silhouette') }
+	);
+	function getHashFilter() {
+	  // get filter=filterName
+	  var matches = location.hash.match( /filter=([^&]+)/i );
+	  var hashFilter = matches && matches[1];
+	  return hashFilter && decodeURIComponent( hashFilter );
+	}
 
-    <hr>
+	$( function() {
 
-    <?php snippet('adaptations') ?>
-	
+	  var $grid = $('.grid');
+
+	  // bind filter button click
+	  var $filterButtonGroup = $('.filter-button-group');
+	  $filterButtonGroup.on( 'click', 'button', function() {
+	    var filterAttr = $( this ).attr('data-filter');
+	    // set filter in hash
+	    location.hash = 'filter=' + encodeURIComponent( filterAttr );
+	  });
+
+	  var isIsotopeInit = false;
+
+	  function onHashchange() {
+	    var hashFilter = getHashFilter();
+	    if ( !hashFilter && isIsotopeInit ) {
+	      return;
+	    }
+	    isIsotopeInit = true;
+	    // filter isotope
+	    $grid.isotope({
+	      itemSelector: '.element-item',
+	      layoutMode: 'fitRows',
+	      filter: hashFilter
+	    });
+	    // set selected class on button
+	    if ( hashFilter ) {
+	      $filterButtonGroup.find('.active').removeClass('active');
+	      $filterButtonGroup.find('[data-filter="' + hashFilter + '"]').addClass('active');
+	    }
+	  }
+
+	  $(window).on( 'hashchange', onHashchange );
+
+	  // trigger event handler to init Isotope
+		var $container = $('.grid');
+		$container.imagesLoaded(function () {
+			onHashchange();
+		});
+
+	});
+	</script>
+
 	<hr>
 
 	<h2>Things That Don't Work (Yet)</h2>
