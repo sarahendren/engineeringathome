e80638cce37cd13374eb3e9b98da1a77be0e80be
Casey Gollan <hello@caseyagollan.com>
2016-01-08 15:23:08 +0100

    contact page formatting

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -2,34 +2,34 @@
 
 <div class="container">
 
-<main class="main" role="main">
+	<main class="main" role="main">
 
-<h1><?php echo $page->title()->html() ?></h1>
+		<h1><?php echo $page->title()->html() ?></h1>
 
-<form action="<?php echo $page->url()?>#form" method="post">
+		<form action="<?php echo $page->url()?>#form" method="post">
+			<div class="form-group">
+				<label for="name" class="required">Name</label>
+				<input<?php e($form->hasError('name'), ' class="has-warning"')?> type="text" name="name" id="name" placeholder="Jane Doe" value="<?php $form->echoValue('name') ?>" class="form-control" required/>
+			</div>
+			<label for="email" class="required">E-Mail</label>
+			<input<?php e($form->hasError('_from'), ' class="has-warning"')?> type="email" name="_from" id="email" placeholder="jane.doe@example.com" value="<?php $form->echoValue('_from') ?>" class="form-control" required/>
 
-<label for="name" class="required">Name</label>
-<input<?php e($form->hasError('name'), ' class="erroneous"')?> type="text" name="name" id="name" value="<?php $form->echoValue('name') ?>" required/>
+			<label for="message">Message</label>
+			<textarea name="message" id="message" class="form-control" placeholder="Your message here..."><?php $form->echoValue('message') ?></textarea>
 
-<label for="email" class="required">E-Mail</label>
-<input<?php e($form->hasError('_from'), ' class="erroneous"')?> type="email" name="_from" id="email" value="<?php $form->echoValue('_from') ?>" required/>
+			<label class="uniform__potty" for="website">Please leave this field blank</label>
+			<input type="text" name="website" id="website" class="uniform__potty" />
 
-<label for="message">Message</label>
-<textarea name="message" id="message"><?php $form->echoValue('message') ?></textarea>
+			<a name="form"></a>
+			<?php if ($form->hasMessage()): ?>
+			<div class="message <?php e($form->successful(), 'success' , 'error')?>">
+			<?php $form->echoMessage() ?>
+			</div>
+			<?php endif; ?>
 
-<label class="uniform__potty" for="website">Please leave this field blank</label>
-<input type="text" name="website" id="website" class="uniform__potty" />
+			<button type="submit" class="btn btn-default" name="_submit" value="<?php echo $form->token() ?>"<?php e($form->successful(), ' disabled')?>>Submit</button>
 
-<a name="form"></a>
-<?php if ($form->hasMessage()): ?>
-<div class="message <?php e($form->successful(), 'success' , 'error')?>">
-<?php $form->echoMessage() ?>
-</div>
-<?php endif; ?>
-
-<button type="submit" name="_submit" value="<?php echo $form->token() ?>"<?php e($form->successful(), ' disabled')?>>Submit</button>
-
-</form>
-</main>
+		</form>
+	</main>
 
 <?php snippet('footer') ?>
