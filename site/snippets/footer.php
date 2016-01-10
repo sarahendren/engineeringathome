046abe791c1b7fa9f6be01ead57c9bce6a4b27e2
Casey Gollan <hello@caseyagollan.com>
2016-01-10 19:09:52 +0100

    google analytics

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,24 +1,24 @@
-<footer class="footer" role="contentinfo">
-	<div class="container">
-		<div class="row">
-			<div class="contact col-sm-4">
-				<h2>Contact <i class="fa fa-envelope"></i></h2>
-				<hr>
-				<?php echo $site->contact()->kt() ?>
+	<footer class="footer" role="contentinfo">
+		<div class="container">
+			<div class="row">
+				<div class="contact col-sm-4">
+					<h2>Contact <i class="fa fa-envelope"></i></h2>
+					<hr>
+					<?php echo $site->contact()->kt() ?>
+				</div>
+				<div class="social col-sm-4">
+					<h2>Connect <i class="fa fa-facebook"></i></h2>
+					<hr>
+					<?php echo $site->social()->kt() ?>
+				</div>
+				<div class="copyright col-sm-4">
+					<h2>Re-use <i class="fa fa-creative-commons"></i></h2>
+					<hr>
+					<?php echo $site->copyright()->kt() ?>
+				</div>	
 			</div>
-			<div class="social col-sm-4">
-				<h2>Connect <i class="fa fa-facebook"></i></h2>
-				<hr>
-				<?php echo $site->social()->kt() ?>
-			</div>
-			<div class="copyright col-sm-4">
-				<h2>Re-use <i class="fa fa-creative-commons"></i></h2>
-				<hr>
-				<?php echo $site->copyright()->kt() ?>
-			</div>	
 		</div>
-	</div>
-</footer>
+	</footer>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"></script>
 	<script>
 	  (function(d) {
@@ -32,6 +32,13 @@
 	$(function() {
 		FastClick.attach(document.body);
 	});
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-72234537-1', 'auto');
+	ga('send', 'pageview');
 	</script>
 </body>
 </html>
