c3fa5014064b46271f4e9d6ae2f0f690f9d5e1e1
Casey Gollan <casey@bullshit.systems>
2016-01-25 22:47:11 -0500

    fix ogp

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,17 +7,17 @@
   <meta name="description" content="<?php echo $site->intro() ?>">
   <meta name="keywords" content="<?php echo $site->keywords() ?>">
   <meta property="og:title" content="<?php if($page->slug() != 'home'): ?><?php echo $page->title()->html() ?> | <?php endif ?><?php echo $site->title()->html() ?>" />
-  <meta property="og:description" content="<?php echo $page->intro() ?>" />
+  <?php if ($page->intro()): ?><meta property="og:description" content="<?php echo $page->intro() ?>" /><?php endif ?>
   <meta property="og:site_name" content="<?php echo $site->title() ?>" />
   <meta property="og:url" content="<?php echo $page->url() ?>" />
-  <meta property="og:image" content="<?php echo $page->images()->first()->url() ?>" />
-  <meta property="og:image:width" content="<?php echo $page->images()->first()->width() ?>" />
-  <meta property="og:image:height" content="<?php echo $page->images()->first()->height() ?>" />
+  <?php if ($page->images()->count() > 0): ?><meta property="og:image" content="<?php echo $page->images()->first()->url() ?>" /><?php endif ?>
+  <?php if ($page->images()->count() > 0): ?><meta property="og:image:width" content="<?php echo $page->images()->first()->width() ?>" /><?php endif ?>
+  <?php if ($page->images()->count() > 0): ?><meta property="og:image:height" content="<?php echo $page->images()->first()->height() ?>" /><?php endif ?>
   <meta name="twitter:card" content="summary" />
   <meta name="twitter:site" content="@unlikelyengineering" />
   <meta name="twitter:title" content="<?php if($page->slug() != 'home'): ?><?php echo $page->title()->html() ?> | <?php endif ?><?php echo $site->title()->html() ?>" />
-  <meta name="twitter:description" content="<?php echo $page->intro() ?>" />
-  <meta name="twitter:image" content="<?php echo $page->images()->first()->url() ?>" />
+  <?php if ($page->description()): ?><meta name="twitter:description" content="<?php echo $page->intro() ?>" /><?php endif ?>
+  <?php if ($page->images()->count() > 0): ?><meta name="twitter:image" content="<?php echo $page->images()->first()->url() ?>" /><?php endif ?>
   <script>document.documentElement.className += ' wf-loading';</script>
   <?php echo css('/assets/css/default.css') ?>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
