37dfadfb0e0f99ee3966071b044134274ebbca55
Casey Gollan <casey@bullshit.systems>
2016-02-11 16:39:04 -0500

    clean up footer and smooth scroll to top

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -23,6 +23,22 @@
 	$(function() {
 		FastClick.attach(document.body);
 	});
+
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
--- site/templates/adaptation.php
+++ site/templates/adaptation.php
@@ -410,20 +410,5 @@ $('.grid a').hover(
   function(myClass){ $(this).find('img').addClass('silhouette') },
   function(myClass){ $(this).find('img').removeClass('silhouette') }
 );
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
 </script>
 <?php snippet('footer') ?>
